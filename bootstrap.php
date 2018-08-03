ba07b91f55a02fe18c2468a06aefc7edf6828525
Ben Batschelet <batschel@adobe.com>
2018-08-03 10:09:00 -0500

    Trim everything down to just PHP

--- bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-define('BP', __DIR__);
-define('DATA', __DIR__ . '/data');
-
-return require __DIR__ . '/vendor/autoload.php';
